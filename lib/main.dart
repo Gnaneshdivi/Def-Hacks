00073f4351b7652b369dfced835e05fe891ae192
Gnaneshdivi <gnanesh_divi@srmap.edu.in>
2020-11-15 22:55:41 +0530

    commit one

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:law/dashboard.dart';
-import 'package:law/doc.dart';
 
 void main() => runApp(App());
 
